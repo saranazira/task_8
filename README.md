09732102c6cb53a072c6eeff9bec4e4526a00702
saranazira <naziradohwa@gmail.com>
2023-01-19 20:41:34 +0600

    task_8 done

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 
 ## Opisanie
 
+<!-- x = int(input('Enter first number: '))
+y = int(input('Enter first number: '))
+z = int(input('Enter first number: '))
+ -->
+
+
+<!-- Ex 2 -->
---
+++ main.py
@@ -0,0 +1,9 @@
+# Zadanie 1
+
+a = int(input('Enter first number: '))
+b = int(input('Enter first number: '))
+
+if a < 0 and b < 0:
+    print('Oba chisla otricatelnie')
+elif a > -1 and b > -1:
+    print('Oba chisla polojitelnie')
